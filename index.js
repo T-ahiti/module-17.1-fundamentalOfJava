a7abeabc5cba469e211f7f2b87c96d415eeb8be7
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-05-27 22:23:12 +0600

    third commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var numbers = [45, 53, 89, 7.6, 30, 98, 56];
+// index start hoy 0 theke.. 45=0th position
+// get element by index
+console.log(numbers[0])
+
+var element = numbers[1];
+console.log(element)
+
+// jodi kono position er number change korte chai
+numbers[3] = 77;
+// etar mane 7.6 er jaygay 77 hbe
+console.log(numbers);
