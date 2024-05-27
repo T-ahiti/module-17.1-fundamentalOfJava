fbaa9e6ed761b089265326d861dec5c84c4a5dd0
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-05-28 00:33:23 +0600

    4th commit

--- comparison.js
+++ comparison.js
@@ -7,5 +7,15 @@
 // console.log(5 != 6);
 // console.log(15 != 15);
 
+// less than equal
+// console.log(5 <= 6);
+// console.log(16 <= 6);
 
+// greater than or equal
+// console.log(16 >= 6);
 
+// multiple condition
+// money1 > money2 && result1 > result2 && height1 > height2
+
+// jekono ekta condition holei hobe bujhanor jnno ||
+// money1 > money2 || result1 > result2 || height1 > height2
