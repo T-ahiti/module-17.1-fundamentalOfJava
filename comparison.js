2bcf5ededeaf9edfeb276cad2d7d19651c3b436f
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-05-28 00:25:38 +0600

    comparison added

---
+++ comparison.js
@@ -0,0 +1,11 @@
+// console.log(5 < 6);
+// console.log(5 > 6);
+// console.log(5 == 6);
+// console.log(6 == 6);
+
+// == soman soman kina ask kore. != 'soman na' kina ask kore
+// console.log(5 != 6);
+// console.log(15 != 15);
+
+
+
