3eeac6e899f3074b0bbbaa1152ec85c736855802
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-05-28 00:12:52 +0600

    unshift

--- push-pop.js
+++ push-pop.js
@@ -2,13 +2,19 @@ var numbers = [78, 88, 25, 326, 843, 6, 75]
 // console.log(numbers.length)
 
 // kono extra number array te jog korte chaile, automatically last element hoye jog hobe
-numbers.push(63);
-console.log(numbers);
+// numbers.push(63);
+// console.log(numbers);
 
 
 
 //last element subtract korte chaile, as eta kaj ei kore last element er upor tai ar number or element er value likha lagtesena, js auto bujhtese je last element ke ber kore dite hbe
-numbers.pop();
-numbers.pop();
-var eliment = numbers.pop();
-console.log(eliment);
+// numbers.pop();
+// numbers.pop();
+// numbers.unshift(23);
+// var eliment = numbers.pop();
+// console.log(eliment);
+
+// unshift diye array er first e number dhukano jay
+
+numbers.unshift(23);
+console.log(numbers);
