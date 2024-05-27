c0110a9f533c03c4e35ef1debdb024e88b320bba
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-05-28 01:05:57 +0600

    6th commit

--- conditional.js
+++ conditional.js
@@ -1,7 +1,20 @@
 var iphonePrice = 79000;
 var myBudget = 9500;
-// if iphone price is less than my budget, I will buy
-/**
- * if 
+/** if iphone price is less than my budget, I will buy
  * 
- * **/ 
+ * if (condition) {
+ *     // will execute if the condition is true
+ * }
+ * 
+ **/ 
+
+
+// etay console kono ans dekhabena karon budget kom
+// if (iphonePrice < myBudget){
+    // console.log('iphn kinbo')
+// }
+
+myBudget = 95000;
+if (iphonePrice < myBudget){
+    console.log('iphn kinbo')
+}
