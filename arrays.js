12d29c542d0f9ba64536f6ccdaa6c9f815d7fd9d
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-05-27 21:25:22 +0600

    2nd commit

---
+++ arrays.js
@@ -0,0 +1,8 @@
+var frndsAge = [11, 21, 45, 17, 106, 14] ;
+var picnicFee = [5000, 2000, 100, 1000, 250]
+// length diye koyta element ache array r modhhe oita bole
+console.log(picnicFee.length)
+
+var nayikas = ['mahi', 'opu', 'shabana', 'tumpa', 'laila']
+var vowels = ['a', 'e', 'i', 'o', 'u']
+console.log(vowels);
