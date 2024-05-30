80d4d6a685782322de7afe148220c76e4fcfef87
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-05-30 14:50:15 +0600

    third commit

--- multiple-condition.js
+++ multiple-condition.js
@@ -1,8 +1,8 @@
 // ekta = sign mane ami value declare kortesi, ==/=== double/triple means ask kortesi(comparison), if*
-var isGratuated = true;
-var salary =25000;
-var cars = 0;
-
+// var isGratuated = true;
+// var salary =25000;
+// var cars = 0;
+// 
 // if(isGratuated == true){
     // console.log('esho biye kore feli')
 // }
@@ -17,9 +17,20 @@ var cars = 0;
     // console.log('tor kopale biye nai')
 // }
 
-if(isGratuated == true && salary > 50000  && cars >= 1){
-    console.log('esho biye kore feli')
+// if(isGratuated == true && salary > 50000  && cars >= 1){
+    // console.log('esho biye kore feli')
+// }
+// else {
+    // console.log('tor kopale biye nai')
+// }
+
+var isGratuated = false;
+var salary =25000;
+var cars = 1;
+
+if(isGratuated == true || salary > 50000 || cars >=1){
+    console.log('eso prem kori')
 }
 else {
-    console.log('tor kopale biye nai')
+    console.log('tomar frnd er mobile number deo')
 }
