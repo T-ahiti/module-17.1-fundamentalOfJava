bf817980fa7851d87193be5621e3f2912b8ef836
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-05-30 14:35:58 +0600

    first commit

---
+++ multiple-condition.js
@@ -0,0 +1,24 @@
+// ekta = sign mane ami value declare kortesi, ==/=== double/triple means ask kortesi(comparison), if*
+var isGratuated = true;
+var salary =25000
+
+// if(isGratuated == true){
+    // console.log('esho biye kore feli')
+// }
+// else {
+    // console.log('tor kopale biye nai')
+// }
+
+// if(isGratuated == true && salary > 50000){
+    // console.log('esho biye kore feli')
+// }
+// else {
+    // console.log('tor kopale biye nai')
+// }
+
+if(isGratuated == true && salary > 50000){
+    console.log('esho biye kore feli')
+}
+else {
+    console.log('tor kopale biye nai')
+}
