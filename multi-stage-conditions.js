c9591350d97454a8db25a28c8c73222c6e7f8086
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-05-31 01:37:46 +0600

    first commit

---
+++ multi-stage-conditions.js
@@ -0,0 +1,17 @@
+var money =10;
+var danish = 45;
+var bread =35;
+var biscuit = 20;
+
+if (danish < money){
+    console.log('danish den')
+}
+else if(bread < money){
+    console.log('bread den')
+}
+else if (biscuit < money){
+    console.log('biscuit den')
+}
+else {
+    console.log('thak onnodin kinbo')
+}
